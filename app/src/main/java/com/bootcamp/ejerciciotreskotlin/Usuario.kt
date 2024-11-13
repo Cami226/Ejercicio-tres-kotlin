6ca39f927a35b895df1b64aa12970492a1d13b3f
[Camila] <cagv.cg@gmail.com>
2024-11-12 22:18:29 -0400

    EJERCICIO 4- ACTUALIZADO-Continuacion de Proyecto Tres Kotlin-

--- app/src/main/java/com/bootcamp/ejerciciotreskotlin/Usuario.kt
+++ app/src/main/java/com/bootcamp/ejerciciotreskotlin/Usuario.kt
@@ -21,12 +21,69 @@ class Usuario(
     }
 }
 
-fun main() {
-    // Crear dos instancias de Usuario
-    val usuario1 = Usuario("Rick", 40, "Sheriff")
-    val usuario2 = Usuario("Walter", 30, referencia = usuario1)
+class GeneradorLista{
 
-    // Mostrar los datos de los usuarios
-    usuario1.mostrarDatos()
-    usuario2.mostrarDatos()
+    private val listaUsuarios = mutableListOf<Usuario>()
+
+
+    fun agregarUsuario(usuario: Usuario) {
+        listaUsuarios.add(usuario)
+        println("${usuario.nombre} ha sido agregado a la lista.")
+    }
+
+    fun eliminarUsuario(nombre: String) {
+        val usuario = listaUsuarios.find { it.nombre == nombre }
+
+        if (usuario != null) {
+            listaUsuarios.remove(usuario)
+            println("$nombre ha sido eliminado de la lista.")
+        } else {
+            println("Usuario con el nombre $nombre no encontrado.")
+        }
+    }
+
+    fun mostrarLista() {
+        if (listaUsuarios.isEmpty()) {
+            println("La lista de usuarios está vacía.")
+        } else {
+            println("La lista de usuarios es:")
+            listaUsuarios.forEach { it.mostrarDatos() }
+        }
+    }
 }
+
+
+fun main() {
+    // Crear la instancia del manejador de usuarios
+    val generLista = GeneradorLista()
+
+    // Crear cinco usuarios
+    val usuario1 = Usuario("Rick Riordan", 45, "Sheriff")
+    val usuario2 = Usuario("Walter White", 60, referencia = usuario1)
+    val usuario3 = Usuario("John Wick", 39, "Cazador")
+    val usuario4 = Usuario("Clark Kent", 34)
+    val usuario5 = Usuario("Peter Parker", 15, "Estudiante", referencia = usuario3)
+
+    // Agregar los usuarios a la lista
+    generLista.agregarUsuario(usuario1)
+    generLista.agregarUsuario(usuario2)
+    generLista.agregarUsuario(usuario3)
+    generLista.agregarUsuario(usuario4)
+    generLista.agregarUsuario(usuario5)
+
+    // Mostrar la lista de usuarios
+    println("\nLista de usuarios:")
+    generLista.mostrarLista()
+
+    // Eliminar un usuario y mostrar la lista nuevamente
+    println("\nEliminando al usuario 'Clark Kent':")
+    generLista.eliminarUsuario("Clark Kent")
+
+    println("\nLista de usuarios actualizada:")
+    generLista.mostrarLista()
+}
+
+
+
+
+
